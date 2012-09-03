6ec41f9c27f2b46a7c9f28e3fa97e2facb2ccb90
Henry 007E <henry@dxconcept.com>
2012-09-03 02:30:26 -0700

    Constants and SmsReceiver class for Buzz feature and code cleanup.

---
+++ src/com/hn/linky/Constants.java
@@ -0,0 +1,44 @@
+package com.hn.linky;
+
+public class Constants 
+{
+	public static final String SHARED_PREF_LINKED_NUMBER = "linkedNumber";
+	public static final String SHARED_PREF_SOURCE_NUMBER = "sourceNumber";
+	public static final String SHARED_PREF_IS_LOGGED_IN = "isLoggedIn";
+	
+	public static final String ACTION_SEND_MESSAGE = "com.hn.linky.action.SEND_MESSAGE";
+	public static final String ACTION_SEND_BUZZ = "com.hn.linky.action.SEND_BUZZ";
+	public static final String ACTION_AUTHENTICATE_BUZZ = "com.hn.linky.action.AUTHENTICATE_BUZZ";
+	public static final String ACTION_UPDATE_DRUNK_LEVEL = "com.hn.linky.action.UPDATE_DRUNK_LEVEL";
+	public static final String ACTION_UPDATE_SLEEPY_LEVEL = "com.hn.linky.action.UPDATE_SLEEPY_LEVEL";
+	public static final String ACTION_UPDATE_MWAH_LEVEL = "com.hn.linky.action.UPDATE_MWAH_LEVEL";
+	public static final String ACTION_UPDATE_HUGGLE_LEVEL = "com.hn.linky.action.UPDATE_HUGGLE_LEVEL";		
+	public static final String ACTION_UPDATE_ALL_LEVELS = "com.hn.linky.action.UPDATE_ALL_LEVELS";
+	public static final String ACTION_WIDGET_UPDATE_DRUNK_LEVEL = "com.hn.linky.action.WIDGET_UPDATE_DRUNK_LEVEL";	
+	public static final String ACTION_WIDGET_UPDATE_SLEEPY_LEVEL = "com.hn.linky.action.WIDGET_UPDATE_SLEEPY_LEVEL";
+	public static final String ACTION_INSTAPIC = "com.hn.linky.action.INSTAPIC";
+	
+	public static final String EXTRA_MESSAGE = "messageType";
+	public static final String EXTRA_DRUNK_LEVEL = "drunkLevel";
+	public static final String EXTRA_SLEEPY_LEVEL = "sleepyLevel";
+	public static final String EXTRA_MWAH_LEVEL = "mwahLevel";
+	public static final String EXTRA_HUGGLE_LEVEL = "huggleLevel";
+	public static final String EXTRA_SMS_MESSAGE = "smsMessage";
+	public static final String EXTRA_ORIGINATING_ADDRESS = "originatingAddress";
+	
+	public static final String BUZZ_KEY = "LINKY BUZZ!";
+	
+	public static final int MESSAGE_POKE = 0;	
+	public static final int MESSAGE_KISS = 1;
+	public static final int MESSAGE_HUG = 2;
+	public static final int MESSAGE_TICKLE = 3;
+	public static final int MESSAGE_HOLDHANDS = 4;
+	public static final int MESSAGE_MISSESYOU = 5;
+	public static final int MESSAGE_SMILE_LOW = 6;
+	public static final int MESSAGE_SMILE_MEDIUM = 7;
+	public static final int MESSAGE_SMILE_HIGH = 8;
+	public static final int MESSAGE_WIDGET_POKE = 9;
+	public static final int MESSAGE_WIDGET_KISS = 10;
+	public static final int MESSAGE_WIDGET_HUG = 11;
+	public static final int MESSAGE_WIDGET_TICKLE = 12;
+}
---
+++ src/com/hn/linky/SmsReceiver.java
@@ -0,0 +1,53 @@
+package com.hn.linky;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class SmsReceiver extends BroadcastReceiver
+{
+	private final String stringToCompare = "LINKY BUZZ!";
+	
+	@Override
+	public void onReceive(Context context, Intent intent) 
+    {	
+		//Get the SMS message passed in
+        Bundle bundle = intent.getExtras();        
+        SmsMessage[] messages = null;
+        String message = null;
+        String origin = null;
+        
+        if (bundle != null)
+        {
+            //Retrieve the SMS message received
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            messages = new SmsMessage[pdus.length];     
+            
+            for (int i = 0; i < messages.length; i++)
+            {
+                messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                
+                message = messages[i].getMessageBody().toString();                
+                
+                if (message.contains(stringToCompare))
+                {
+                	abortBroadcast();
+                	
+                	origin = messages[i].getOriginatingAddress();
+                	
+                	Intent authenticateIntent = new Intent(context, LinkyIntentService.class);
+                	authenticateIntent.setAction(Constants.ACTION_AUTHENTICATE_BUZZ);
+                	authenticateIntent.putExtra(Constants.EXTRA_SMS_MESSAGE, message);
+                	authenticateIntent.putExtra(Constants.EXTRA_ORIGINATING_ADDRESS, origin);
+                	context.startService(authenticateIntent);
+                }
+                
+                // str += "SMS from " + messages[i].getOriginatingAddress();
+                // str += messages[i].getMessageBody().toString();
+                        
+            }
+        }
+    }
+}
