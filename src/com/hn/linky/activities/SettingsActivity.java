80bfbcfe0a343d51d26328055a9bdeb841f4e81c
Henry 007E <henry@dxconcept.com>
2012-09-16 20:05:52 -0700

    INCOMPLETE COMMIT.

--- src/com/hn/linky/activities/SettingsActivity.java
+++ src/com/hn/linky/activities/SettingsActivity.java
@@ -1,21 +1,34 @@
 package com.hn.linky.activities;
 
 import com.hn.linky.R;
+import com.hn.linky.utilities.Constants;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener 
-{
-	public static final String KEY_PREF_SYNC_CONN = "pref_syncConnectionType";
-	
-    @Override
+{	
+	@Override
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings_preferences);
+        
+        Preference linkedNumber = (Preference) findPreference("linkedNumberPref");
+        //linkedNumber.onDialogClosed(true);
+        
+    }
+	
+	public void onStart(Bundle savedInstanceState) 
+    {	
+		
+        
     }
 
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) 
@@ -23,4 +36,22 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 		
 		
 	}
+	
+	
+    String ListPreference;
+    String linkedNumberPreference;
+    
+    String customPref;
+
+    private void getPrefs() 
+    {
+            // Get the xml/preferences.xml preferences
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+            linkedNumberPreference = preferences.getString(Constants.SHARED_PREF_LINKED_NUMBER, null);
+           
+            
+            // Get the custom preference
+            //SharedPreferences mySharedPreferences = getSharedPreferences("myCustomSharedPrefs", Activity.MODE_PRIVATE);
+            //customPref = mySharedPreferences.getString("myCusomPref", "");
+    }
 }
